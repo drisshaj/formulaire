8505286824dbcebfa7ce41d713ab90b11b9d8bda
driss senj <drisshaj2004@gmail.com>
2022-11-30 08:44:00 +0000

    modification

--- Form1.cs
+++ Form1.cs
@@ -8,159 +8,21 @@ using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 using System.Data;
 using Microsoft.Win32;
-using WinFormsApp2.LoginSysten;
+
 using MySqlX.XDevAPI.Common;
 using System.Collections;
+using LoginSysten;
+using MySqlX.XDevAPI.Relational;
 
 namespace WinFormsApp2
-
-{
-    namespace LoginSysten
-    {
-        public class Config
-        {
-            string ConectionString = "";  // save connection string
-            public MySqlConnection connection = null;
-            public string server = "127.0.0.1";// MySQL host / ip of the computer
-            public string user = "root";// MySQL user
-            public string password = "";// MySQL password 
-            DataSet ds;
-            DataTable dt;
-            public string Table = "livre"; // initialize db table
-            public string ConnectionType = "";
-            string RecordSource = "";
-
-            DataGridView tempdata;
-
-            public Config()
-            {
-
-            }
-
-            // function to connect to the database
-            public void Connect(string database_name)
-            {
-                try
-                {
-
-                    ConectionString = "SERVER=" + server + ";" + "DATABASE=" + database_name + ";" + "UID=" + user + ";" + "PASSWORD=" + password + ";";
-
-                    connection = new MySqlConnection(ConectionString);
-                }
-                catch (Exception E)
-                {
-                    MessageBox.Show(E.Message);
-                }
-            }
-
-            // Function to execute select statements
-            public void ExecuteSql(string Sql_command)
-            {
-
-                nowquiee(Sql_command);
-
-            }
-
-            // creates connection to MySQL before execution
-            public void nowquiee(string sql_comm)
-            {
-                try
-                {
-                    MySqlConnection cs = new MySqlConnection(ConectionString);
-                    cs.Open();
-                    MySqlCommand myc = new MySqlCommand(sql_comm, cs);
-                    myc.ExecuteNonQuery();
-                    cs.Close();
-
-
-                }
-                catch (Exception err)
-                {
-
-                    MessageBox.Show(err.Message);
-                }
-            }
-
-            // function to execute delete , insert and update
-            public void Execute(string Sql_command)
-            {
-                RecordSource = Sql_command;
-                ConnectionType = Table;
-                dt = new DataTable(ConnectionType);
-                try
-                {
-                    string command = RecordSource.ToUpper();
-
-                    //======================if sql contains select==========================================
-                    MySqlDataAdapter da2 = new MySqlDataAdapter(RecordSource, connection);
-
-                    DataSet tempds = new DataSet();
-                    da2.Fill(tempds, ConnectionType);
-                    da2.Fill(tempds);
-
-                    //======================================================================================
-
-
-                }
-                catch (Exception err) { MessageBox.Show(err.Message); }
-            }
-
-            // function to bring selected results based on column name and row index
-            public string Results(int ROW, string COLUMN_NAME)
-            {
-                try
-                {
-                    return dt.Rows[ROW][COLUMN_NAME].ToString();
-                }
-                catch (Exception err)
-                {
-                    MessageBox.Show(err.Message);
-                    return "";
-
-                }
-            }
-
-            // function to bring selected results based on column index and row index
-            
-
-            // Execute select statement
-            public void ExecuteSelect(string Sql_command)
-            {
-                RecordSource = Sql_command;
-                ConnectionType = Table;
-
-                dt = new DataTable(ConnectionType);
-                try
-                {
-                    string command = RecordSource.ToUpper();
-                    MySqlDataAdapter da = new MySqlDataAdapter(RecordSource, connection);
-                    ds = new DataSet();
-                    da.Fill(ds, ConnectionType);
-                    da.Fill(dt);
-                    tempdata = new DataGridView();
-                }
-                catch (Exception err)
-                {
-                    MessageBox.Show(err.Message);
-                }
-
-
-            }
-
-            // count Number of rows after selecy
-            public int Count()
-            {
-                return dt.Rows.Count;
-            }
-        }
-    }
+{ 
     public partial class Form1 : Form
     {
         Config db = new Config();
         public Form1()
         {
             InitializeComponent();
-            db.Connect("userdata");
+            db.Connect("livre");
         }
      
         private void button2_Click_1(object sender, EventArgs e)
@@ -191,24 +53,34 @@ namespace WinFormsApp2
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            db.Execute("UPDATE livre(nom_livre,prix)" +
+               "WHERE  PRIX " + "prix ='" + textBox2.Text + "'");
 
             listBox1.Items.Remove(listBox1.SelectedItem);
-           // int index = listBox1.SelectedIndex;
-           // listBox2.Items.RemoveAt(index);
-            
-            
+            // int index = listBox1.SelectedIndex;
+            // listBox2.Items.RemoveAt(index);
+            db.Execute("DELETE FROM livre(nom_livre,prix)" +
+                "WHERE  nom_livre " + "nom_livre ='" + listBox1.SelectedItem + "'");
+            // db.Execute("DELETE FROM livre(nom_livre,prix)" +
+            // "WHERE  Prix " + "prix ='" + listBox2.Items.RemoveAt(index) + "'");
+
 
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             ListBox listbox1 = new ListBox();
+
             listBox1.Items.Add("rythem of war");
             listBox1.Items.Add("oethbringer");
             listBox1.Items.Add("words of raidiance");
             listBox1.Items.Add("the way of kings");
+           // var Result = Table.where().tolist();
+            //foreach (var item in Result)
+            //{
+              //  listBox1.Items.Add(item.nom_livre);
+            //}
 
-            
+
             this.Controls.Add(listBox1);
         }
 
@@ -216,6 +88,7 @@ namespace WinFormsApp2
         {
             listBox1.Items.Clear();
             listBox2.Items.Clear();
+            db.Execute("DELETE * FROM livre(nom_livre,prix)" + "'");
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -237,6 +110,141 @@ namespace WinFormsApp2
             listBox2.Items.Add("mille");
 
 
+        }
+    }
+}
+namespace LoginSysten
+{
+    public class Config
+    {
+        string ConectionString = "";
+        public MySqlConnection connection = null;
+        public string server = "127.0.0.1";
+        public string user = "root";
+        public string password = "";
+        DataSet ds;
+        DataTable dt;
+        public string Table = "livre";
+        public string ConnectionType = "";
+        string RecordSource = "";
+
+        DataGridView tempdata;
+
+        public Config()
+        {
+
+        }
+
+        // function to connect to the database
+        public void Connect(string database_name)
+        {
+            try
+            {
+
+                ConectionString = "SERVER=" + server + ";" + "DATABASE=" + database_name + ";" + "UID=" + user + ";" + "PASSWORD=" + password + ";";
+
+                connection = new MySqlConnection(ConectionString);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        // Function to execute select statements
+        public void ExecuteSql(string Sql_command)
+        {
+
+            nowquiee(Sql_command);
+
+        }
+
+        // creates connection to MySQL before execution
+        public void nowquiee(string sql_comm)
+        {
+            try
+            {
+                MySqlConnection cs = new MySqlConnection(ConectionString);
+                cs.Open();
+                MySqlCommand myc = new MySqlCommand(sql_comm, cs);
+                myc.ExecuteNonQuery();
+                cs.Close();
+
+
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message);
+            }
+        }
+
+
+        public void Execute(string Sql_command)
+        {
+            RecordSource = Sql_command;
+            ConnectionType = Table;
+            dt = new DataTable(ConnectionType);
+            try
+            {
+                string command = RecordSource.ToUpper();
+
+
+                MySqlDataAdapter da2 = new MySqlDataAdapter(RecordSource, connection);
+
+                DataSet tempds = new DataSet();
+                da2.Fill(tempds, ConnectionType);
+                da2.Fill(tempds);
+
+
+
+            }
+            catch (Exception err) { MessageBox.Show(err.Message); }
+        }
+
+
+        public string Results(int ROW, string COLUMN_NAME)
+        {
+            try
+            {
+                return dt.Rows[ROW][COLUMN_NAME].ToString();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return "";
+
+            }
+        }
+
+
+        public void ExecuteSelect(string Sql_command)
+        {
+            RecordSource = Sql_command;
+            ConnectionType = Table;
+
+            dt = new DataTable(ConnectionType);
+            try
+            {
+                string command = RecordSource.ToUpper();
+                MySqlDataAdapter da = new MySqlDataAdapter(RecordSource, connection);
+                ds = new DataSet();
+                da.Fill(ds, ConnectionType);
+                da.Fill(dt);
+                tempdata = new DataGridView();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+
+        }
+
+
+        public int Count()
+        {
+            return dt.Rows.Count;
         }
     }
 }
