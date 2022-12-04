7c89cd689df55daa1ab283eba44e0fb3a47d1d22
driss senj <drisshaj2004@gmail.com>
2022-12-04 21:29:15 +0000

    modif

--- Form1.cs
+++ Form1.cs
@@ -9,110 +9,208 @@ using System.Windows.Forms;
 using System.Data;
 using Microsoft.Win32;
 
+
 using MySqlX.XDevAPI.Common;
 using System.Collections;
 using LoginSysten;
 using MySqlX.XDevAPI.Relational;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.ComponentModel.DataAnnotations;
 
 namespace WinFormsApp2
-{ 
+{
     public partial class Form1 : Form
     {
         Config db = new Config();
         public Form1()
         {
             InitializeComponent();
-            db.Connect("livre");
+            db.Connect("user");
         }
-     
+
         private void button2_Click_1(object sender, EventArgs e)
         {
-            Environment.Exit(0);
+            button1.Visible = true;
+            button2.Visible = true;
+            button2.Visible = true;
+            
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            db.Execute("INSERT INTO livre (nom_livre,prix) " +
-                "VALUES nom_livre='" + textBox1.Text + "' , prix ='" + textBox2.Text + "'");
-           
-                bool flag = listBox1.Items.Contains(textBox1);
-            if (this.listBox1.Items.Contains(textBox1.Text))
+            if (checkBox1.Checked == true)
+            { 
+                db.Execute("INSERT INTO user (nom) " +
+                    "VALUES nom='" + textBox1.Text + "'");
+
+
+            bool flag = comboBox1.Items.Contains(textBox1);
+            if (this.comboBox1.Items.Contains(textBox1.Text))
             {
-                MessageBox.Show("book already existing");
+                MessageBox.Show("user already existing");
+
             }
             else
             {
-                MessageBox.Show("livre ajouter");
-                listBox1.Items.Add(textBox1.Text);
-                listBox2.Items.Add(textBox2.Text);
+                MessageBox.Show("user added");
+                comboBox1.Items.Add(textBox1.Text);
+
             }
-            
-          
+            }
+            else if(checkBox3.Checked == true)
+            {
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+
+                db.Execute("DELETE FROM user(nom)" +
+                    "WHERE  nom " + "nom ='" + comboBox1.SelectedItem + "'");
+            }
+            else if(checkBox2.Checked==true)
+            {
+                db.Execute("UPDATE user(nom)" +
+                   "WHERE  nom " + "nom ='" + textBox1.Text + "'");
+                comboBox1.Items[comboBox1.SelectedIndex] = textBox1.Text;
+                MessageBox.Show("le nom  à été changer");
+            }
+
+
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            db.Execute("UPDATE livre(nom_livre,prix)" +
-               "WHERE  PRIX " + "prix ='" + textBox2.Text + "'");
-
-            listBox1.Items.Remove(listBox1.SelectedItem);
-            // int index = listBox1.SelectedIndex;
-            // listBox2.Items.RemoveAt(index);
-            db.Execute("DELETE FROM livre(nom_livre,prix)" +
-                "WHERE  nom_livre " + "nom_livre ='" + listBox1.SelectedItem + "'");
-            // db.Execute("DELETE FROM livre(nom_livre,prix)" +
-            // "WHERE  Prix " + "prix ='" + listBox2.Items.RemoveAt(index) + "'");
-
-
-        }
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ListBox listbox1 = new ListBox();
-
-            listBox1.Items.Add("rythem of war");
-            listBox1.Items.Add("oethbringer");
-            listBox1.Items.Add("words of raidiance");
-            listBox1.Items.Add("the way of kings");
-           // var Result = Table.where().tolist();
-            //foreach (var item in Result)
-            //{
-              //  listBox1.Items.Add(item.nom_livre);
-            //}
-
-
-            this.Controls.Add(listBox1);
-        }
+        
+       
 
         private void button5_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-            listBox2.Items.Clear();
-            db.Execute("DELETE * FROM livre(nom_livre,prix)" + "'");
+            comboBox1.Items.Clear();
+            comboBox1.Items.Clear();
+            db.Execute("DELETE * FROM user(nom)" + "'");
         }
 
-        private void button4_Click(object sender, EventArgs e)
+       
+        bool textboxChanged = false;
+        /* private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+         {
+             if (!textboxChanged)
+             {
+                 checkbox2.visible=false;
+                 checkbox3.visible=false;
+
+             }
+             else if(textboxChanged)
+             {
+
+                    checkbox2.visible=true;
+                 checkbox3.visible=true;
+
+
+         }
+        */
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-           
-            db.Execute("UPDATE livre(nom_livre,prix)" +
-               "WHERE  PRIX "+ "prix ='" + textBox2.Text + "'");
-            listBox2.Items[listBox2.SelectedIndex] = textBox2.Text;
-            MessageBox.Show("le prix à été changer");
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+           db.ExecuteSelect("SELECT * FROM `user` " );
+            for (int i = 0; i < db.Count(); i++)
+
+            {
+
+                comboBox1.Items.Add(db.Results(i,0) + " " + db.Results(i,1) + " " + db.Results(i,2));
+
+            }
+            if (string.IsNullOrEmpty(comboBox1.Text))
+            {
+                button1.Visible = true;
+                checkBox2.Visible = true;
+                checkBox3.Visible = true;
+
+            }
+            else
+            {
+                button1.Visible = false;
+               
+
+
+            }
+
+
 
         }
 
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            ListBox listbox2 = new ListBox();
-            listBox2.Items.Add("cent");
-            listBox2.Items.Add("cent_dix");
-            listBox2.Items.Add("cinque-cent");
-            listBox2.Items.Add("mille");
+            if(checkBox1.Checked==true)
+            {
+                checkBox2.Visible = false;
+                checkBox3.Visible = false;  
 
+            }
+            else if(checkBox1.Checked==false)
+            {
+                checkBox2.Visible = true;
+                checkBox3.Visible=true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox1.Visible = false;
+                checkBox3.Visible = false;
+               
+                
+
+            }
+            else if (checkBox2.Checked == false)
+            {
+                checkBox1.Visible = true;
+                button1.Visible = true;
+                checkBox3.Visible = true;
+                button2.Visible = true;
+            }
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                checkBox1.Visible = false;
+                checkBox2.Visible = false;
+               
+               
+
+            }
+            else if (checkBox2.Checked == false)
+            {
+                checkBox1.Visible = true;
+                button1.Visible = true;
+                checkBox2.Visible = true;
+                checkBox3.Visible = true;
+                button2.Visible = true;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_2(object sender, EventArgs e)
+        {
 
         }
     }
-}
+    }
+
 namespace LoginSysten
 {
     public class Config
@@ -124,7 +222,7 @@ namespace LoginSysten
         public string password = "";
         DataSet ds;
         DataTable dt;
-        public string Table = "livre";
+        public string Table = "user";
         public string ConnectionType = "";
         string RecordSource = "";
 
@@ -151,7 +249,7 @@ namespace LoginSysten
             }
         }
 
-        // Function to execute select statements
+       
         public void ExecuteSql(string Sql_command)
         {
 
@@ -159,7 +257,7 @@ namespace LoginSysten
 
         }
 
-        // creates connection to MySQL before execution
+        // creates con to MySQL  
         public void nowquiee(string sql_comm)
         {
             try
@@ -202,22 +300,6 @@ namespace LoginSysten
             catch (Exception err) { MessageBox.Show(err.Message); }
         }
 
-
-        public string Results(int ROW, string COLUMN_NAME)
-        {
-            try
-            {
-                return dt.Rows[ROW][COLUMN_NAME].ToString();
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message);
-                return "";
-
-            }
-        }
-
-
         public void ExecuteSelect(string Sql_command)
         {
             RecordSource = Sql_command;
@@ -240,11 +322,42 @@ namespace LoginSysten
 
 
         }
+        public string Results(int ROW, int COLUMN_NAME)
+        {
+            try
+            {
+                return dt.Rows[ROW][COLUMN_NAME].ToString();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return dt.Rows[ROW][COLUMN_NAME].ToString();
 
-
+            }
+        }
         public int Count()
         {
             return dt.Rows.Count;
         }
+        
     }
+   /* public DataSet validateUserName()
+    {
+        MySqlConnection cs = new MySqlConnection(ConectionString);
+        cs.Open();
+        MySqlCommand dbCommand = new MySqlCommand();
+        var stm = "SELECT COUNT(*) FROM user  WHERE nom= '" + TextBox1.text + '";
+            dbCommand.command(stm);
+        // this textusername is from the window form
+        dt = new DataTable(ConnectionType);
+        MySqlDataAdapter da = new MySqlDataAdapter(RecordSource, connection);
+        DataSet ds = new DataSet();
+        da = new MySqlDataAdapter();
+        da.SelectCommand = dbCommand;
+        da.Fill(ds);
+        return ds;
+        cs.Close();
+
+    }
+   */
 }
